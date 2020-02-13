0ec6b18a75421c0e3962c4ba85a645fda440c9be
tae-lim <taelim92@gmail.com>
2020-02-13 15:42:45 -0800

    Add content to About page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,13 +2,41 @@ import * as React from 'react';
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome!</h1>
-      <h1>I'm so glad that you're here!</h1>
-      <p>I am someone that always learning. Cliche, I know, but I wanted to build a web application with technologies I've never used before</p>
-      <p>In order to develop this web app, I learned and integrated Typescript, React Hooks, Context API, SASS, on the Frontend</p>
-      <p>On the backend, I learned Python, Flask, SASS, and deployed it on AWS!</p>
-      <p>I would love to learn more! In fact, my next plan would be to implement GraphQL!</p>
+    <div className="about-container">
+      <div className="about-intro">
+        <h1>Welcome,</h1>
+        <h1>I'm glad you're here!</h1>
+      </div>
+      <div className="about-section-container">
+        <div className="about-section">
+          <p>I've always had an enthusiasm for technology growing up in Silicon Valley.</p>
+          <p>I built my first desktop in middle school, took Java in high school, and built cryptocurrency mining rigs during undergrad.</p>
+          <p>Programming was only a hobby in lieu of other interests, but I began taking it seriously in 2018.</p>
+        </div>
+        <div className="about-section">
+          <p>I love being stimulated and engaged through challenging and interesting problems</p>
+          <p>I always want to learn new languages, technologies, and methodologies so I developed this portfolio with a stack I've never used!</p>
+          <p>My future plans for this portfolio is to integrate GraphQL and the Apollo Client!</p>
+        </div>
+        <div className="about-section">
+          <p>"Learn lots. Don't judge. Laugh for no reason. Be nice. Seek fulfillment."</p>
+          <p>Life should be lived to the fullest capacity and part of that stems from one's satisfaction in their work, relationships, and accomplishments.</p>
+          <p>Exercise and meditation are a daily habit of mine and it allows me to untap my creative and logical potential.</p>
+        </div>
+      </div>
+      <div className="about-stack">
+        <div className="about-stack-info">
+          <div>Placeholder</div>
+          <h6>Front End</h6>
+          <div>Typescript, React Hooks, SASS, Context API</div>
+        </div>
+        <span className="about-stack-divider"></span>
+        <div className="about-stack-info">
+          <div>Placeholder</div>
+          <h6>Back End</h6>
+          <div>Python, Flask, AWS</div>
+        </div>
+      </div>
     </div>
   );
 };
