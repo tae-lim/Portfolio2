d8df09adcbd40ffa40ead31f709e7fa300d317c6
tae-lim <taelim92@gmail.com>
2020-02-13 13:49:14 -0800

    Integrate Vanta library

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,43 +1,28 @@
 import * as React from 'react';
-import * as ReactTransitionGroup from 'react-transition-group';
+import WAVES from 'vanta/dist/vanta.waves.min';
+import * as THREE from 'three';
+//import * as ReactTransitionGroup from 'react-transition-group';
 
 const duration = 8000;
 
 const Home: React.FC = () => {
+  const [vantaEffect, setVantaEffect] = React.useState(0);
+  const myRef = React.useRef(null);
 
-  // const [nameIntro, setNameIntro] = React.useState(false);
-  // const [swDevIntro, setSwDevIntro] = React.useState(false);
-  // const [occupationsIntro, setOccupationsIntro] = React.useState(false);
-
-  // React.useEffect(() => {
-  //   setNameIntro(true);
-  //   setSwDevIntro(true);
-  //   setOccupationsIntro(true);
-  // });
+  React.useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(WAVES({
+        el: myRef.current,
+        THREE: THREE
+      }));
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    }
+  }, [vantaEffect]);
 
   return (
-    <div className="home-container">
-      {/* <ReactTransitionGroup.CSSTransition 
-        in={nameIntro} 
-        timeout={duration}
-        classNames="alert"
-      >
-
-      </ReactTransitionGroup.CSSTransition>
-      <ReactTransitionGroup.CSSTransition 
-        in={swDevIntro} 
-        timeout={12000}
-        classNames="alert"
-      >
-
-      </ReactTransitionGroup.CSSTransition>
-      <ReactTransitionGroup.CSSTransition 
-        in={occupationsIntro} 
-        timeout={18000}
-        classNames="alert"
-      >
-
-      </ReactTransitionGroup.CSSTransition> */}
+    <div className="home-container" ref={myRef}>
       <h1 className="animated animatedFadeInUp fadeInUp">Hi, I'm Tae Lim</h1>
       <h2 className="animated animatedFadeInUp fadeInUp">Software Developer</h2>
       <h4 className="animated animatedFadeInUp fadeInUp">Frontend | Backend | Fullstack</h4>
@@ -46,3 +31,36 @@ const Home: React.FC = () => {
 }
 
 export default Home;
+
+
+// const [nameIntro, setNameIntro] = React.useState(false);
+// const [swDevIntro, setSwDevIntro] = React.useState(false);
+// const [occupationsIntro, setOccupationsIntro] = React.useState(false);
+
+// React.useEffect(() => {
+//   setNameIntro(true);
+//   setSwDevIntro(true);
+//   setOccupationsIntro(true);
+// });
+
+/* <ReactTransitionGroup.CSSTransition 
+  in={nameIntro} 
+  timeout={duration}
+  classNames="alert"
+>
+
+</ReactTransitionGroup.CSSTransition>
+<ReactTransitionGroup.CSSTransition 
+  in={swDevIntro} 
+  timeout={12000}
+  classNames="alert"
+>
+
+</ReactTransitionGroup.CSSTransition>
+<ReactTransitionGroup.CSSTransition 
+  in={occupationsIntro} 
+  timeout={18000}
+  classNames="alert"
+>
+
+</ReactTransitionGroup.CSSTransition> */
