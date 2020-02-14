c8f4c0a15b903e16e10e1a259beac9be06a5b0c7
tae-lim <taelim92@gmail.com>
2020-02-14 14:18:27 -0800

    Add temporary styling and background for About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react';
+import NET from 'vanta/dist/vanta.net.min';
+import * as THREE from 'three';
 
 const About: React.FC = () => {
+  const [vantaEffect, setVantaEffect] = React.useState(0);
+  const myRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(NET({
+        el: myRef.current,
+        THREE: THREE
+      }));
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    }
+  }, [vantaEffect]);
+
   return (
-    <div className="about-container">
+    <div className="about-container" ref={myRef}>
       <div className="about-intro">
         <h1>Welcome,</h1>
         <h1>I'm glad you're here!</h1>
@@ -14,14 +31,9 @@ const About: React.FC = () => {
           <p>Programming was only a hobby in lieu of other interests, but I began taking it seriously in 2018.</p>
         </div>
         <div className="about-section">
-          <p>I love being stimulated and engaged through challenging and interesting problems</p>
+          <p>I love being stimulated and engaged through challenging and interesting problems and take deep personal satisfaction in my work, relationships, and accomplishments</p>
           <p>I always want to learn new languages, technologies, and methodologies so I developed this portfolio with a stack I've never used!</p>
           <p>My future plans for this portfolio is to integrate GraphQL and the Apollo Client!</p>
-        </div>
-        <div className="about-section">
-          <p>"Learn lots. Don't judge. Laugh for no reason. Be nice. Seek fulfillment."</p>
-          <p>Life should be lived to the fullest capacity and part of that stems from one's satisfaction in their work, relationships, and accomplishments.</p>
-          <p>Exercise and meditation are a daily habit of mine and it allows me to untap my creative and logical potential.</p>
         </div>
       </div>
       <div className="about-stack">
