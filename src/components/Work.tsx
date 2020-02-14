19233b45148dbade184bd492d32ddd7f85142206
tae-lim <taelim92@gmail.com>
2020-02-14 14:17:50 -0800

    Create Work component skeleton

---
+++ src/components/Work.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import * as THEME from 'three';
+
+const Work: React.FC = () => {
+  return (
+    <div>
+      <div className="work-container">
+        <div>
+          <div>
+            VPS
+          </div>
+          <div>
+            Info about VPS
+          </div>
+        </div>
+      </div>
+
+      <hr></hr>
+
+      <div className="project-container">
+        <div>Greenway</div>
+        <div>BackPacker</div>
+        <div>CookBuk</div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
