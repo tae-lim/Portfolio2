6ae43987d7d85f31ef5166635744b5a37582501e
tae-lim <taelim92@gmail.com>
2020-02-15 16:39:09 -0800

    Add Background for Skills component

--- src/components/BubblesParticles.tsx
+++ src/components/BubblesParticles.tsx
@@ -8,12 +8,56 @@ class BubblesParticles extends React.Component {
       <div className="particle-canvas">
         <Particles
           params={{
-            particles: {
-              color: {
-                value: "#000000"
+            "particles": {
+              "number": {
+                "value": 160,
+                "density": {
+                  "enable": false
+                }
+              },
+              "size": {
+                "value": 4,
+                "random": true,
+                "anim": {
+                  "speed": 6,
+                  "size_min": 0.4
+                }
+              },
+              "line_linked": {
+                "enable": false
+              },
+              "move": {
+                "random": true,
+                "speed": 1,
+                "direction": "top",
+                "out_mode": "out"
+              }
+            },
+            "interactivity": {
+              "events": {
+                "onhover": {
+                  "enable": true,
+                  "mode": "bubble"
+                },
+                "onclick": {
+                  "enable": true,
+                  "mode": "repulse"
+                }
+              },
+              "modes": {
+                "bubble": {
+                  "distance": 250,
+                  "duration": 2,
+                  "size": 0,
+                  "opacity": 0
+                },
+                "repulse": {
+                  "distance": 400,
+                  "duration": 4
+                }
               }
             }
-          }}
+          }} 
         />
       </div>
     );
