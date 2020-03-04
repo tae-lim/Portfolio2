fb73f081179fdce682d6b338881fa7541b5073cf
tae-lim <taelim92@gmail.com>
2020-03-04 14:17:29 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(bodyParser.json());
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'))
+})
+
 app.listen(port, () => {
   console.log(`Listening on Port: ${port}`);
 });
