0e40ecec1a9ec705ecd84e970d03112f136a7713
tae-lim <taelim92@gmail.com>
2020-03-04 14:22:05 -0800

    Revert changes

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(bodyParser.json());
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'))
-})
-
 app.listen(port, () => {
   console.log(`Listening on Port: ${port}`);
 });
