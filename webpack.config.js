facc7c29d421d495812e0b67c4a2823866e9c30e
tae-lim <taelim92@gmail.com>
2020-03-04 18:27:03 -0800

    revert changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const config = {
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, '/dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/'
   },
